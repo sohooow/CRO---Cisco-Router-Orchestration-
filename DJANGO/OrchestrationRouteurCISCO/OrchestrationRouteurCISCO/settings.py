f13c9a66ab37c372fc413e4f7e0289ed6824674d
b3rnvrd <eliottcolson.snir@gmail.com>
2025-01-15 09:19:28 +0100

    debut creation conteneur
    ajout dockerfile
    ajout docker-compose.yml

--- DJANGO/OrchestrationRouteurCISCO/OrchestrationRouteurCISCO/settings.py
+++ DJANGO/OrchestrationRouteurCISCO/OrchestrationRouteurCISCO/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/5.1/ref/settings/
 """
 
 from pathlib import Path
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -76,8 +77,13 @@ WSGI_APPLICATION = 'OrchestrationRouteurCISCO.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': os.getenv('MYSQL_DATABASE', 'mydatabase'),
+        'USER': os.getenv('MYSQL_USER', 'user'),
+        'PASSWORD': os.getenv('MYSQL_PASSWORD', 'password'),
+        'HOST': os.getenv('DB_HOST', 'db'),  # Le nom du service Docker pour MySQL
+        'PORT': '3306',
+        'OPTIONS': {'charset': 'utf8mb4',}
     }
 }
 
