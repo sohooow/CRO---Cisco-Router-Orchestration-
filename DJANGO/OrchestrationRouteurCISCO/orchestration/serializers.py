456840b3c53c81e1ff4d52c93fbde92d6dfdf7ca
Tifenne Jupiter <tifenne.jupiter@telecom-sudparis.eu>
2025-03-24 17:32:57 +0100

    Résolution des conflits et suppression des fichiers inutiles

--- DJANGO/OrchestrationRouteurCISCO/orchestration/serializers.py
+++ DJANGO/OrchestrationRouteurCISCO/orchestration/serializers.py
@@ -6,20 +6,20 @@ from .models import Router, User, Interface, Log
 class RouterSerializer(serializers.ModelSerializer) : 
     class Meta : 
         model = Router
-        fields ='_all_'
+        fields ="_all_"
 
 
 class UserSerializer(serializers.ModelSerializer) : 
     class Meta : 
         model = User
-        fields ='_all_'
+        fields ="_all_"
 
 class InterfaceSerializer(serializers.ModelSerializer) : 
     class Meta : 
         model = Interface
-        fields ='_all_'
+        fields ="_all_"
 
 class LogSerializer(serializers.ModelSerializer) : 
     class Meta : 
         model = Log
-        fields ='_all_'
+        fields ="_all_"
