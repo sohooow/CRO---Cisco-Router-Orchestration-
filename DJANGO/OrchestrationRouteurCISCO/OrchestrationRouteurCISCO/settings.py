7f6547e440140ea13693a324a8b86b8f82fe17d6
Tifenne Jupiter <tifenne.jupiter@telecom-sudparis.eu>
2025-04-30 12:06:53 +0200

    Update settings.py
    Résolution de conflits avec les settings.py

--- DJANGO/OrchestrationRouteurCISCO/OrchestrationRouteurCISCO/settings.py
+++ DJANGO/OrchestrationRouteurCISCO/OrchestrationRouteurCISCO/settings.py
@@ -10,8 +10,10 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/5.1/ref/settings/
 """
 
-import os
 from pathlib import Path
+import os
+
+
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -21,87 +23,91 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = "django-insecure-ap#(^d&1e7r9%*bxw02@qd*hg7^zcik@o1++$k1g*d*5ph@1m^"
+SECRET_KEY = 'django-insecure-ap#(^d&1e7r9%*bxw02@qd*hg7^zcik@o1++$k1g*d*5ph@1m^'
+
+
 
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
-ALLOWED_HOSTS = ["127.0.0.1", "localhost", "web"]
+ALLOWED_HOSTS = ['127.0.0.1', 'localhost', 'web']
 
 
 # Application definition
 
 INSTALLED_APPS = [
     "orchestration.apps.OrchestrationConfig",
-    "django.contrib.admin",
-    "django.contrib.auth",
-    "django.contrib.contenttypes",
-    "django.contrib.sessions",
-    "django.contrib.messages",
-    "django.contrib.staticfiles",
-    "corsheaders",
-    "django_extensions",
-    "rest_framework",
-    "debug_toolbar",
+    'django.contrib.admin',
+    'django.contrib.auth',
+    'django.contrib.contenttypes',
+    'django.contrib.sessions',
+    'django.contrib.messages',
+    'django.contrib.staticfiles',
+    'corsheaders',
+    'django_extensions', 
+    'rest_framework',
+    'debug_toolbar',
 ]
 
 MIDDLEWARE = [
-    "django.middleware.security.SecurityMiddleware",
-    "django.contrib.sessions.middleware.SessionMiddleware",
-    "django.middleware.common.CommonMiddleware",
-    "django.middleware.csrf.CsrfViewMiddleware",
-    "django.contrib.auth.middleware.AuthenticationMiddleware",
-    "django.contrib.messages.middleware.MessageMiddleware",
-    "django.middleware.clickjacking.XFrameOptionsMiddleware",
-    "corsheaders.middleware.CorsMiddleware",
-    "django_htmx.middleware.HtmxMiddleware",
-    "debug_toolbar.middleware.DebugToolbarMiddleware",
+    'django.middleware.security.SecurityMiddleware',
+    'django.contrib.sessions.middleware.SessionMiddleware',
+    'django.middleware.common.CommonMiddleware',
+    'django.middleware.csrf.CsrfViewMiddleware',
+    'django.contrib.auth.middleware.AuthenticationMiddleware',
+    'django.contrib.messages.middleware.MessageMiddleware',
+    'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'corsheaders.middleware.CorsMiddleware',
+    'django_htmx.middleware.HtmxMiddleware',
+    'debug_toolbar.middleware.DebugToolbarMiddleware',
+
 ]
 
-ROOT_URLCONF = "OrchestrationRouteurCISCO.urls"
+ROOT_URLCONF = 'OrchestrationRouteurCISCO.urls'
 
 REST_FRAMEWORK = {
-    "DEFAULT_PERMISSION_CLASSES": [
-        "rest_framework.permissions.AllowAny",
+    'DEFAULT_PERMISSION_CLASSES': [
+        'rest_framework.permissions.AllowAny', 
     ]
 }
 
 
+
 TEMPLATES = [
     {
-        "BACKEND": "django.template.backends.django.DjangoTemplates",
-        "DIRS": [BASE_DIR / "templates/"],
-        "APP_DIRS": True,
-        "OPTIONS": {
-            "context_processors": [
-                "django.template.context_processors.debug",
-                "django.template.context_processors.request",
-                "django.contrib.auth.context_processors.auth",
-                "django.contrib.messages.context_processors.messages",
+        'BACKEND': 'django.template.backends.django.DjangoTemplates',
+        'DIRS': [BASE_DIR / "templates/"],
+        'APP_DIRS': True,
+        'OPTIONS': {
+            'context_processors': [
+                'django.template.context_processors.debug',
+                'django.template.context_processors.request',
+                'django.contrib.auth.context_processors.auth',
+                'django.contrib.messages.context_processors.messages',
             ],
         },
     },
 ]
 
-WSGI_APPLICATION = "OrchestrationRouteurCISCO.wsgi.application"
+WSGI_APPLICATION = 'OrchestrationRouteurCISCO.wsgi.application'
 
 
 # Database
 # https://docs.djangoproject.com/en/5.1/ref/settings/#databases
 
 DATABASES = {
-    "default": {
-        "ENGINE": "django.db.backends.mysql",
-        "NAME": "orchestration_routeur",
-        "USER": "root",
-        "PASSWORD": "rootpassword",
-        "HOST": "mysql_db",
-        "PORT": "3306",
+    'default': {
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'orchestration_routeur',
+        'USER': 'root',
+        'PASSWORD': 'rootpassword',
+        'HOST': 'mysql_db',
+        'PORT': '3306',
     }
 }
 
 
-AUTH_USER_MODEL = "orchestration.User"
+AUTH_USER_MODEL = 'orchestration.User'
 
 
 # Password validation
@@ -109,16 +115,16 @@ AUTH_USER_MODEL = "orchestration.User"
 
 AUTH_PASSWORD_VALIDATORS = [
     {
-        "NAME": "django.contrib.auth.password_validation.UserAttributeSimilarityValidator",
+        'NAME': 'django.contrib.auth.password_validation.UserAttributeSimilarityValidator',
     },
     {
-        "NAME": "django.contrib.auth.password_validation.MinimumLengthValidator",
+        'NAME': 'django.contrib.auth.password_validation.MinimumLengthValidator',
     },
     {
-        "NAME": "django.contrib.auth.password_validation.CommonPasswordValidator",
+        'NAME': 'django.contrib.auth.password_validation.CommonPasswordValidator',
     },
     {
-        "NAME": "django.contrib.auth.password_validation.NumericPasswordValidator",
+        'NAME': 'django.contrib.auth.password_validation.NumericPasswordValidator',
     },
 ]
 
@@ -126,9 +132,9 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/5.1/topics/i18n/
 
-LANGUAGE_CODE = "en-us"
+LANGUAGE_CODE = 'en-us'
 
-TIME_ZONE = "UTC"
+TIME_ZONE = 'UTC'
 
 USE_I18N = True
 
@@ -138,29 +144,27 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
 
-STATIC_URL = "/static/"
+STATIC_URL = '/static/'
 STATICFILES_DIRS = [
-    os.path.join(
-        BASE_DIR, "orchestration/static"
-    ),  # Spécifie où se trouvent les fichiers statiques
+    os.path.join(BASE_DIR, 'orchestration/static'),  # Spécifie où se trouvent les fichiers statiques
 ]
-STATIC_ROOT = (
-    BASE_DIR / "staticfiles"
-)  # Dossier pour collecter les fichiers statiques lors de la collecte
+STATIC_ROOT = BASE_DIR / "staticfiles"  # Dossier pour collecter les fichiers statiques lors de la collecte
 
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
 
-DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
+DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
-# Utilisation du modèle Utilisateur personnalisé
-AUTH_USER_MODEL = "orchestration.User"
-LOGIN_REDIRECT_URL = "/config"  # Page d'accueil ou page spécifique
-LOGOUT_REDIRECT_URL = "/login/"  # Page de connexion ou page d'accueil
-LOGIN_URL = "/login/"  # URL de la page de connexion
-LOGOUT_URL = "/logout/"  # URL de la page de déconnexion
+#Utilisation du modèle Utilisateur personnalisé 
+AUTH_USER_MODEL = 'orchestration.User'
+LOGIN_REDIRECT_URL = '/config'  # Page d'accueil ou page spécifique
+LOGOUT_REDIRECT_URL = '/login/'  # Page de connexion ou page d'accueil
+LOGIN_URL = '/login/'            # URL de la page de connexion
+LOGOUT_URL = '/logout/'          # URL de la page de déconnexion
 
 INTERNAL_IPS = [
-    "127.0.0.1",  # Localhost
+    '127.0.0.1',  # Localhost
 ]
+
+DEFAULT_ROUTER_IP = "172.16.10.11"
