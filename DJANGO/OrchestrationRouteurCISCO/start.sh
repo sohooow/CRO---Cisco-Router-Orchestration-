49290a61dd3ae3720c60f1e45b41c3c7d8757d32
Tifenne Jupiter <tifenne.jupiter@telecom-sudparis.eu>
2025-06-30 15:30:02 +0200

    Ajout du dump SQL et modifications de docker/start

--- DJANGO/OrchestrationRouteurCISCO/start.sh
+++ DJANGO/OrchestrationRouteurCISCO/start.sh
@@ -2,19 +2,19 @@
 
 echo "Lancement de l'environnement Docker..."
 
-docker-compose build
-docker-compose up -d
+docker compose build
+docker compose up -d
 
 echo "Attente de la base de données..."
 sleep 10
 
 echo "Création automatique des fichiers de migrations..."
-docker-compose exec web python manage.py makemigrations --noinput
+docker compose exec web python manage.py makemigrations --noinput
 
 echo "Migration de la base de données..."
-docker-compose exec web python manage.py migrate --noinput
+docker compose exec web python manage.py migrate --noinput
 
 echo "Collecte des fichiers statiques..."
-docker-compose exec web python manage.py collectstatic --noinput
+docker compose exec web python manage.py collectstatic --noinput
 
 echo "Serveur prêt sur http://localhost:8080"
