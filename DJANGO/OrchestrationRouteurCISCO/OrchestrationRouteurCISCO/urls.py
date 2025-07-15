d085a50f1e39e6259ae4937e3992db5329e1242b
b3rnvrd <eliottcolson.snir@gmail.com>
2025-07-15 09:37:22 +0200

    Test patch isort/black

--- DJANGO/OrchestrationRouteurCISCO/OrchestrationRouteurCISCO/urls.py
+++ DJANGO/OrchestrationRouteurCISCO/OrchestrationRouteurCISCO/urls.py
@@ -31,5 +31,5 @@ if settings.DEBUG:
     urlpatterns += [
         path("__debug__/", include(debug_toolbar.urls)),
     ]
-    
+
 urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
--- DJANGO/OrchestrationRouteurCISCO/orchestration/admin.py
+++ DJANGO/OrchestrationRouteurCISCO/orchestration/admin.py
@@ -6,15 +6,19 @@ from .models import Interface, Log, Router, User
 
 class RouterAdmin(admin.ModelAdmin):
     def has_add_permission(self, request):
-        return request.user.is_superuser or getattr(request.user, "role", None) == "admin"
+        return (
+            request.user.is_superuser or getattr(request.user, "role", None) == "admin"
+        )
 
     def has_change_permission(self, request, obj=None):
-        return request.user.is_superuser or getattr(request.user, "role", None) == "admin"
+        return (
+            request.user.is_superuser or getattr(request.user, "role", None) == "admin"
+        )
 
     def has_delete_permission(self, request, obj=None):
-        return request.user.is_superuser or getattr(request.user, "role", None) == "admin"
-
-
+        return (
+            request.user.is_superuser or getattr(request.user, "role", None) == "admin"
+        )
 
 
 admin.site.register(Router, RouterAdmin)
